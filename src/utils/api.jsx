7cf6064e1bcc15c028bd9285b25dafabde298f4c
Omkar Dixit <omkardixit@Pritchetts-MacBook-Pro.local>
2018-06-19 21:05:43 -0500

    First commit

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=f0c6e238ca78d929c57118c8088e07c4';
+
+module.exports = {
+    get: function(place) {
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // No need for special headers
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        });
+    }
+};
